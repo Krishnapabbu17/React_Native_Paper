ca9a5252efaec2d56de62732fc4ee5aa22089132
Krishnapabbu17 <krishnapabbu7@gmail.com>
2025-09-17 07:51:58 -0500

    smth

--- React_Paper/app/index.tsx
+++ React_Paper/app/index.tsx
@@ -1,15 +1,10 @@
-import { Text, View } from "react-native";
-
+import MyComponent from "@/components/myComponent";
+import defaultStyles from "@/styles/defaultStyles";
+import { View } from "react-native";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {defaultStyles.screen}>
+      <MyComponent />
     </View>
   );
 }
