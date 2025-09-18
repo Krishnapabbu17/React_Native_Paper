cafd4b87c065fbe523e20daab29488401a4eb6c8
Krishnapabbu17 <krishnapabbu7@gmail.com>
2025-09-18 07:54:27 -0500

    finished code!!!!!!!!!!!!!

--- React_Paper/components/newComponent.tsx
+++ React_Paper/components/newComponent.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { FlatList, Image, StyleSheet, View } from 'react-native';
 import { BottomNavigation, Text, useTheme } from 'react-native-paper';
 
-// quick reusable grid
 const TileGrid = ({ items }: { items: string[] }) => (
   <FlatList
     data={items}
@@ -16,38 +15,38 @@ const TileGrid = ({ items }: { items: string[] }) => (
   />
 );
 
-// simple demo image lists (use your own URLs when ready)
+
 const ANIMALS = [
-  'https://picsum.photos/seed/animal1/800/800',
-  'https://picsum.photos/seed/animal2/800/800',
-  'https://picsum.photos/seed/animal3/800/800',
-  'https://picsum.photos/seed/animal4/800/800',
-  'https://picsum.photos/seed/animal5/800/800',
-  'https://picsum.photos/seed/animal6/800/800',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDEf2x48OO7avhX3sDnPcxx-2UelTwMABUWg&s',
+  'https://www.science.org/do/10.1126/science.zjv23mz/abs/_20214_on_adolesence_animals_virginia.jpg',
+  'https://thumbs.dreamstime.com/b/regal-horse-galloping-misty-meadow-dawn-exudes-aura-freedom-refinement-set-against-serene-backdrop-398466658.jpg',
+  'https://potterparkzoo.org/wp-content/uploads/2017/01/Kota-Lion-2023-1-640x480.jpg',
+  'https://cdn.creatureandcoagency.com/uploads/2014/09/autumn-animals-wolf.jpg',
+  'https://shop.wwf.ca/cdn/shop/files/giant-panda-WW230176.jpg?v=1694540662&width=1024',
 ];
 const CARS = [
-  'https://picsum.photos/seed/car1/800/800',
-  'https://picsum.photos/seed/car2/800/800',
-  'https://picsum.photos/seed/car3/800/800',
-  'https://picsum.photos/seed/car4/800/800',
-  'https://picsum.photos/seed/car5/800/800',
-  'https://picsum.photos/seed/car6/800/800',
+  'https://imgd.aeplcdn.com/370x208/n/cw/ec/139133/aura-exterior-right-front-three-quarter-8.jpeg?isig=0&q=80',
+  'https://hips.hearstapps.com/hmg-prod/images/2025-honda-cr-v-hybrid-awd-sport-touring-102-679407cb80051.jpg?crop=0.702xw:0.590xh;0.0529xw,0.341xh&resize=2048:*',
+  'https://hips.hearstapps.com/hmg-prod/images/2019-dodge-challenger-srt-hellcat-redeye-comparison-104-1581425446.jpg?crop=0.658xw:0.555xh;0.0896xw,0.293xh&resize=1200:*',
+  'https://hips.hearstapps.com/hmg-prod/images/240314-01-gt-r-my25-009-65f465d1cdbd1.jpg',
+  'https://media.wired.com/photos/5a0e6fe27d6fd0312d8bba41/16:9/w_1978,h_1113,c_limit/Roadster_Hero.jpg',
+  'https://hips.hearstapps.com/hmg-prod/amv-prod-cad-assets/wp-content/uploads/2017/04/cars_3_characters-1.jpg',
 ];
 const FOOD = [
-  'https://picsum.photos/seed/food1/800/800',
-  'https://picsum.photos/seed/food2/800/800',
-  'https://picsum.photos/seed/food3/800/800',
-  'https://picsum.photos/seed/food4/800/800',
-  'https://picsum.photos/seed/food5/800/800',
-  'https://picsum.photos/seed/food6/800/800',
+  'https://hips.hearstapps.com/hmg-prod/images/chicken-curry-index-65a1629f3c1e7.jpg?crop=0.8885082084225553xw:1xh;center,top&resize=1200:*',
+  'https://images.themodernproper.com/production/posts/2016/ClassicCheeseBurger_9.jpg?w=1200&h=1200&q=60&fm=jpg&fit=crop&dm=1749310239&s=463b18fc3bb51dc5d96e866c848527c4',
+  'https://thegirlinspired.com/wp-content/uploads/2024/11/southern-fried-chicken-recipe-card.jpg',
+  'https://www.orchidsandsweettea.com/wp-content/uploads/2019/05/Veggie-Pizza-2-of-5-e1691215701129.jpg',
+  'https://sausagemaker.com/wp-content/uploads/Homemade-French-Fries_8.jpg',
+  'https://www.modernhoney.com/wp-content/uploads/2024/01/Homemade-Chicken-Ramen-1-crop-scaled.jpg',
 ];
 const SPORTS = [
-  'https://picsum.photos/seed/sport1/800/800',
-  'https://picsum.photos/seed/sport2/800/800',
-  'https://picsum.photos/seed/sport3/800/800',
-  'https://picsum.photos/seed/sport4/800/800',
-  'https://picsum.photos/seed/sport5/800/800',
-  'https://picsum.photos/seed/sport6/800/800',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/LeBron_James_%2851959977144%29_%28cropped2%29.jpg/250px-LeBron_James_%2851959977144%29_%28cropped2%29.jpg',
+  'https://i0.wp.com/sportytell.com/wp-content/uploads/2018/11/Ronaldo-Champions-league-Manchester-United.jpg?zoom=1.25&w=680&ssl=1',
+  'https://media.cnn.com/api/v1/images/stellar/prod/231122100352-01-tom-brady-file-101623.jpg?c=16x9&q=h_833,w_1480,c_fill',
+  'https://www.investopedia.com/thmb/cGLTSgopZFDs_WUnDPjxfIsEnfw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1863206916-67a4cf0a1f5c4598b6e7fda26ef7e004.jpg',
+  'https://m.media-amazon.com/images/M/MV5BZGYwYTNjNTAtZTFhNS00MDFlLThmZjUtN2I4ODQ5ZjI5MDU3XkEyXkFqcGc@._V1_.jpg',
+  'https://cdn.shopify.com/s/files/1/0878/2614/files/barry-bonds-edit-1.jpg?7859236886406504759',
 ];
 
 const Animals = () => (
